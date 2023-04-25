d9f2bf1dae817cdbd74fcf16476a817737eedb62
AnthonyTennis <teciorowskianthony3@gmail.com>
2023-04-24 19:50:56 -0700

    Parser working with tests

--- Asgn3.rkt
+++ Asgn3.rkt
@@ -5,50 +5,55 @@
 (require typed/rackunit)
 
 (struct FundefC ([name : Symbol] [arg : Symbol] [body : ExprC]))
-(define-type ExprC (U NumC IdC AppC PlusC SubC MultC DivC leq0))
+(define-type ExprC (U NumC IdC AppC BinopC leq0))
 (struct NumC ([n : Real]))
 (struct IdC ([s : Symbol]))
 (struct AppC ([fun : Symbol] [arg : ExprC]))
-(struct PlusC ([l : ExprC] [r : ExprC]))
-(struct SubC ([l : ExprC] [r : ExprC]))
-(struct MultC ([l : ExprC] [r : ExprC]))
-(struct DivC ([l : ExprC] [r : ExprC]))
+(struct BinopC ([op : binop] [l : ExprC] [r : ExprC]))
 (struct leq0 ([exp : ExprC]))
-(define-type binop (U Symbol))
+(define-type binop (U '+ '- '* '/))
 
+; Lookup table for binary operators
+;(define binop-lookup
+;  (make-immutable-hash
+;   (list
+;     (cons '+ +)
+;     (cons '- -)
+;     (cons '* *)
+;     (cons '/ /)))
+
+
+; ---------------------------------------------------
+; Parser and tests
+
+; the parse function will take in an s-expression and modify it to create an ExprC
 (define (parse [s : Sexp]) : ExprC
-    (cond
-      [(real? s) (NumC s)]
-      [(list? s)
-       (case (first s)
-         [(+) (PlusC (parse (second s)) (parse (third s)))]
-         [() (MultC (parse (second s)) (parse (third s)))]
-         [else (error 'parse "invalid list input")])]
-      [else (error 'parse "invalid input")]))
-
-#;(define (parse-fundef [s : Sexp]) : FundefC
-  )
-
-(define (parse-prog [s : Sexp]) : (Listof FundefC)
-  (list (FundefC 'a 'x (NumC 6))))
-
-(define (interp-fns [funs : (Listof FundefC)]) : Real
-  53)
-
-(define (interp [exp : ExprC][funs : (Listof FundefC)]) : Real
-  (match exp
-    [(NumC n) n]
-    [(PlusC l r) (+ (interp l funs) (interp r funs))]
-    [(MultC l r) ( (interp l funs) (interp r funs))]))
-
-(: top-interp (Sexp -> Real))
-(define (top-interp fun-sexps)
-  (interp-fns (parse-prog fun-sexps)))
-
-
-(define (binop-convert [s : Symbol]) : ExprC
   (match s
-    ['+ (PlusC (NumC 5) (NumC 3))]
-    ['- (SubC (NumC 5) (NumC 3))]
-    ['* (MultC (NumC 5) (NumC 3))]
-    ['/ (DivC (NumC 5) (NumC 3))]))
+    [(? real? n) (NumC n)]
+    [(list op arg1 arg2)
+     (match op
+       [(? (lambda (x) (member x (list '+ '- '* '/)))) (BinopC op (parse arg1) (parse arg2))]
+       [_ (error 'parse "VVQS invalid list input ~e" s)])]
+    [_ (error 'parse "VVQS invalid input ~e" s)]))
+
+
+; Custom comparison function for ExprC instances
+(define (exprc-equal? [e1 : ExprC] [e2 : ExprC]) : Boolean
+  (match* (e1 e2)
+    [((BinopC op1 l1 r1) (BinopC op2 l2 r2)) (and (eq? op1 op2) (exprc-equal? l1 l2) (exprc-equal? r1 r2))]
+    [((NumC n1) (NumC n2)) (= n1 n2)]))
+
+(check-true (exprc-equal? (parse '(+ 1 2)) (BinopC '+ (NumC 1) (NumC 2))))
+(check-true (exprc-equal? (parse '(* 3 4)) (BinopC '* (NumC 3) (NumC 4))))
+(check-true (exprc-equal? (parse '(/ 10 5)) (BinopC '/ (NumC 10) (NumC 5))))
+(check-false (exprc-equal? (parse '(/ 10 5)) (BinopC '- (NumC 10) (NumC 5))))
+(check-exn (regexp (regexp-quote "VVQS invalid input"))
+           (lambda () (parse 'not-a-list-or-num)))
+(check-exn (regexp (regexp-quote "VVQS invalid list input"))
+           (lambda () (parse (cast '(l 2 3) Sexp))))
+
+
+
+; ---------------------------------------------------
+; Interpreter and tests
+
